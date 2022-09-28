d6f10618b627be0a8f770d77b49aca068f794b9e
Ksenia-Dugushova <ksenia.dugushova@mail.ru>
2022-09-28 15:54:24 +0400

    saved changes in Main.fs

--- src/project/Main.fs
+++ src/project/Main.fs
@@ -1,19 +1,35 @@
 namespace MathMech_lessons
 
 open System.Reflection
-open System
-
 
 module Main =
+    // Возведение в степень 
+    let f1 (x:float) (y:int): float =
+            if x=0.0 && y = 0 then
+                printfn "Undefined"
+                0.0
+            elif x = 0.0 then
+                0.0
+            elif x = 1.0 then
+                1.0               
+            else        
+                if y < 0 then
+                    let mutable p = 1.0
+                    for n in 0..-1..y do           
+                        p <- p*(1.0/x)
+                    p
+                elif y = 0 then
+                    1.0
+                else
+                    let mutable k = 1.0
+                    for m in 1..y do           
+                        k <- k*x
+                    k
+
+
 
     [<EntryPoint>]
-    let main (artg: string) =
-    0
-
-
-
-
-
-    let z = x ** 3
-    printfn $"z={z}"
-            0
+    let main (argv: string array) =
+        0
+        
+           
